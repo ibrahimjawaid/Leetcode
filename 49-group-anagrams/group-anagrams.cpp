347ffb0a4fa297340fbd4786dc583c1b2d7da2d1
Ibrahim Jawaid <ibrahimjawaid4@gmail.com>
2026-06-07 20:44:20 -0400

    Time: 12 ms (83.42%) | Memory: 25 MB (78.80%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
-
-    bool areAnagrams(string a, string b) {
-        if (a.length() == b.length()) {
-            int count[26] = {};
-            for (char c : a) count[c - 'a'] += 1;
-            for (char c : b) count[c - 'a'] -= 1;
-            for (int c : count) {
-                if (c != 0) return false;
-            }
-            return true;
-
-        }
-        return false;
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
         vector<vector<string>> result;
-        vector<pair<string, bool>> copy;
-        for (auto s : strs) {
-            pair<string, bool> add{s, false};
-            copy.push_back(add);
+        for (int i = 0; i < strs.size(); ++i) {
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            mp[key].push_back(strs[i]);
         }
 
-        for (int i = 0; i < strs.size(); ++i) {
-            if (copy[i].second == true) continue;
-            vector<string> add {strs[i]};
-            copy[i].second = true;
-            bool flag = false;
-            for (int x = i + 1; x < strs.size(); ++x) {
-                if (areAnagrams(strs[i], strs[x]) && copy[x].second == false) {
-                    add.push_back(strs[x]);
-                    copy[x].second = true;
-                    flag = true;
-                } 
-            }
-            result.push_back(add);
+        for (const auto &pair : mp) {
+            result.push_back(pair.second);
         }
         return result;
     }
